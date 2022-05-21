830cfad8319025eff005ed21dcd566775dffe319
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-21 01:26:06 -0700

    Agregar archivos de proyecto.

---
+++ Ciclos_Iteracion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos_Iteracion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            int resultado;
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Tabla de multiplicar del {0}", i);
+                Console.ReadKey();
+
+                for (j = 1; j <= 10; j++)
+                {
+                    resultado = i * j;
+
+                    Console.WriteLine("{0} * {1} = {2}", i, j, resultado);
+                    Console.ReadKey();
+
+                }
+            }
+        }
+    }
+}
